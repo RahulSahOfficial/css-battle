4865fb42da1b6fd155b972a5ff0310da3fe9f29c
RahulSahOfficial <developersah@gmail.com>
2024-10-07 12:18:51 +0530

    Feat: Add All Challenges Render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import env from "dotenv";
 import pg from "pg";
-import ejs from "ejs";
+import ejs, { render } from "ejs";
 import bodyParser from "body-parser";
 
 env.config();
@@ -29,24 +29,58 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 })
+
+app.get("/challenges",async (req,res)=>{
+  try{
+    const respose = await db.query("SELECT id,name,live FROM challenge WHERE show=true");
+    res.render("challenges.ejs",{
+      data:respose.rows
+    });
+  }
+  catch(err){
+    res.render("challenges.ejs",{
+      data:[]
+    });
+  }
+  
+})
 app.get("/play/:cid",async (req,res)=>{
   const challengeId=req.params.cid;
   try{
     const respose = await db.query("SELECT * FROM challenge WHERE id=$1",[challengeId]);
     if(respose.rows.length==1){
       const challenge=respose.rows[0];
-      const data={
-        challengeId:challenge.id,
-        name:challenge.name,
-        colors:challenge.colors.split(',')
+      if(challenge.live){
+        const data={
+          challengeId:challenge.id,
+          name:challenge.name,
+          colors:challenge.colors.split(',')
+        }
+        res.render("play.ejs",{data});
       }
-      res.render("play.ejs",{data});
+      else//if not live
+        res.render("message.ejs",{
+          data:{
+            heading:"❌ Challenge is not started!",
+            description:"The challenge you're trying to access hasn't started yet. Keep an eye on this space and sharpen your CSS skills in the meantime. Get ready to join the battle once it goes live!"
+          }
+        });
     }
-    else
-      res.render("contest-not-found.ejs",{data});
+    else//if challege not found
+      res.render("message.ejs",{
+        data:{
+          heading:"❌ Challenge not found!",
+          description:"The challenge ID you entered does not exist. Please check the challenge ID or explore other challenges."
+        }
+      });
   }
-  catch(err){
-    res.render("contest-not-found.ejs");
+  catch(err){//if something went wrong
+    res.render("message.ejs",{
+      data:{
+        heading:"❌ Challenge not found!",
+        description:"The challenge ID you entered does not exist. Please check the challenge ID or explore other challenges."
+      }
+    });
   }
 })
 
